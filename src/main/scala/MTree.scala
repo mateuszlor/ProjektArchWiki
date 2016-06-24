949c6f31722288641731f915906de8a5680daf9e
shhatrat <180575@edu.p.lodz.pl>
2016-06-25 00:04:15 +0200

    add MTree

---
+++ src/main/scala/MTree.scala
@@ -0,0 +1,14 @@
+import scala.None
+
+/**
+  * Created by marek on 24.06.16.
+  */
+case class MTree(url : String, parent: Option[MTree] = None){
+
+  def getParents() : String = {
+    parent match {
+      case Some(parent) => url + parent.getParents()
+      case None =>  url
+    }
+  }
+}
--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -10,7 +10,11 @@ import net.ruippeixotog.scalascraper.model.Element
 object MainClass {
  def main(args: Array[String]){
     println("hello")
-   println(getListOfLinks("https://wiki.archlinux.org/index.php/Secure_Boot#Put_firmware_in_.22Setup_Mode.22"))
+//   println(getListOfLinks("https://wiki.archlinux.org/index.php/Secure_Boot#Put_firmware_in_.22Setup_Mode.22"))
+//   print(getLinkToRandomPage())
+
+//    val o = new MTree("a" , Some(new MTree("b" )))
+//   print(o.getParents())
   }
 
   def getListOfLinks(link : String) : List[Option[String]] = {
@@ -18,5 +22,23 @@ object MainClass {
     val doc = browser.get(link)
     val items: List[Element] = doc >> elementList("a")
     items.map(_ >?> attr("href")("a"))
+  }
+
+  def getLinkToRandomPage() : String = {
+    val browser = JsoupBrowser()
+    browser.get("https://wiki.archlinux.org/index.php/Special:Random").location
+  }
+
+  def checkValidLink(url : String) : Boolean = {
+    val browser = JsoupBrowser()
+    try {
+      val doc = browser.get(url)
+      val link = doc.location
+      val items: List[Element] = doc >> elementList("p")
+      link.contains("wiki.archlinux.org")
+    }
+    catch {
+      case e: Exception => false
+    }
   }
 }
