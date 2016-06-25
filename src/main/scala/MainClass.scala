b262ae86eac91b73c9516d718b9d43760b06f02b
shhatrat <180575@edu.p.lodz.pl>
2016-06-25 15:35:13 +0200

    try to implement method "play"

--- src/main/scala/MainClass.scala
+++ src/main/scala/MainClass.scala
@@ -7,6 +7,7 @@ import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.model.Element
 
+import scala.collection.immutable.List
 import scala.collection.mutable.ListBuffer
 import scala.util.matching.Regex
 
@@ -15,11 +16,24 @@ object MainClass {
     println("hello")
 //   println(getListOfLinks("https://wiki.archlinux.org/index.php/Secure_Boot#Put_firmware_in_.22Setup_Mode.22"))
 //   print(getLinkToRandomPage())
-
 //    val o = new MTree("a" , Some(new MTree("b" )))
 //   print(o.getParents())
 
    prepareLinks(getListOfLinks(getLinkToRandomPage())).foreach(println)
+  }
+
+  def play( url1 :String , url2: String) = {
+    if(!url1.equals(url2))
+      {
+        val root  = new Some(MTree(url1))
+        val tmpListOfUrl = prepareLinks(getListOfLinks(root.get.url))
+        val currentLevel = new ListBuffer[MTree] ;
+        val tmpList = new ListBuffer[MTree] ;
+        tmpList :: createTreeListFromList( tmpListOfUrl,root)
+        tmpList.foreach{ x => if(x.equals(currentLevel)) x = None}
+        tmpList.foreach{ x => if(x.url.equals(url2)) print(x.getParents())}
+        currentLevel :: tmpList.toList
+      }
   }
 
   def getListOfLinks(url : String) : List[Option[String]] = {
@@ -27,6 +41,12 @@ object MainClass {
     val doc = browser.get(url)
     val items: List[Element] = doc >> elementList("a")
     items.map(_ >?> attr("href")("a"))
+  }
+
+  def createTreeListFromList(inputList : List[String], mTree: Option[MTree]) : List[MTree] = {
+    val list = new ListBuffer[MTree]
+    inputList.foreach(x => list += new MTree(x , mTree))
+    list.toList
   }
 
   def getLinkToRandomPage() : String = {
@@ -41,7 +61,9 @@ object MainClass {
       case Some(x) =>  l +=  pat findFirstIn x
       case None => ""
     }
-    l.toList.flatten
+    val ww = new ListBuffer[String];
+    val pp = l.toList.flatten.foreach( x =>  ww+=("https://wiki.archlinux.org"+x))
+    ww.toList
   }
 
   def checkValidLink(url : String) : Boolean = {
